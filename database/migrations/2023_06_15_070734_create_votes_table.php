0c922e00e7dd7ff753b5f4922b1384a4a6278f12
Sabbaghh <your_email@example.com>
2023-06-15 16:22:39 +0400

    create initial migration

---
+++ database/migrations/2023_06_15_070734_create_votes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained(); // person who voted
+            $table->foreignId('candidate_id'); // person who was voted for
+            $table->integer('year')->default(date('Y'));
+            $table->integer('month')->default(date('m'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('votes');
+    }
+};
