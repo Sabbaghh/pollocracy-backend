2f2b033b8c1f03b00ffa83f6f1139214989f6bf7
Sabbaghh <your_email@example.com>
2023-06-21 17:27:15 +0400

    Implement Authentication for users

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreUserRequest;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated();
+
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $user->tokens()->delete();
+            $abilities = collect([]);
+            $user->is_admin ? $abilities->push('admin') : null;
+            $user->is_candidate ? $abilities->push('candidate') : null;
+            $data = [
+                "user" => $user,
+                "token" => $user->createToken('auth_token', $abilities->isEmpty() ? [""] : $abilities->all())->plainTextToken
+            ];
+            return $this->success($data, 'User logged in', 200);
+        }
+
+        return $this->fail(null, 'Unauthorized', 401);
+    }
+
+
+    public function register(StoreUserRequest $request)
+    {
+        $request->validated($request->all());
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        $data = [
+            "user" => $user,
+            "token" => $user->createToken('auth_token ' . $user->email)->plainTextToken
+        ];
+        return $this->success($data, 'User created', 200);
+    }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return $this->success(null, 'User logged out', 200);
+    }
+}
